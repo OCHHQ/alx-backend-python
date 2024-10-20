cae2887e42b575dc542ce8c2550f9a5f39b3ef88
OCHHQ <enosejecollins@gmail.com>
2024-10-20 17:05:33 +0100

    concatenation

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 
+"""
+This module contains a function to concatenate two strings.
+"""
+
+
 def concat(str1: str, str2: str) -> str:
     """Return the concatenation of str1 & str2"""
     return str1 + str2
